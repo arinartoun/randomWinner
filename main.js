ddeeebe19ece92e7709af00457963d3c7e39182f
arinartoun <76731903+arinartoun@users.noreply.github.com>
2021-02-08 14:36:37 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+var player1 = Math.floor(Math.random()*6+1);
+var player2 = Math.floor(Math.random()*6+1);
+function showImage(){
+  for (i=0;i<1;i++){
+    if (player1 == 1){
+      document.querySelector('#img1').setAttribute('src','./images/dice1.png')
+    }else if (player1 == 2){
+      document.querySelector('#img1').setAttribute('src','./images/dice2.png')
+    }else if (player1 == 3){
+      document.querySelector('#img1').setAttribute('src','./images/dice3.png')
+    }else if (player1 == 4){
+      document.querySelector('#img1').setAttribute('src','./images/dice4.png')
+    }else if (player1 == 5){
+      document.querySelector('#img1').setAttribute('src','./images/dice5.png')
+    }else if (player1 == 6){
+      document.querySelector('#img1').setAttribute('src','./images/dice6.png')
+     }
+
+   }
+  for (i=1;i<2;i++){
+    if (player2 == 1){
+      document.querySelector('#img2').setAttribute('src','./images/dice1.png')
+    }else if (player2 == 2){
+      document.querySelector('#img2').setAttribute('src','./images/dice2.png')
+    }else if (player2 == 3){
+      document.querySelector('#img2').setAttribute('src','./images/dice3.png')
+    }else if (player2 == 4){
+      document.querySelector('#img2').setAttribute('src','./images/dice4.png')
+    }else if (player2 == 5){
+      document.querySelector('#img2').setAttribute('src','./images/dice5.png')
+    }else if (player2 == 6){
+      document.querySelector('#img2').setAttribute('src','./images/dice6.png')
+     }
+
+  }
+}
+function winDecider(){
+  if(player1 > player2){
+    document.querySelector('.decider').innerHTML= '🚩Player 1 Wins';
+  }else if (player2 > player1){
+    document.querySelector('.decider').innerHTML= 'Player 2 Wins🚩';
+  }else if (player1 == player2){
+    document.querySelector('.decider').innerHTML= '🏳Draw🏳';
+  }
+}
+console.log(player1);
+console.log(player2);
+showImage();
+winDecider();
